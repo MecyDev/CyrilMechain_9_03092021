dbd1def15641e493be0002ad5a7a82cb812c5bfb
macmc <macmecy@gmail.com>
2021-09-13 18:53:30 +0200

    open menu ok

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -26,6 +26,7 @@ export const filteredBills = (data, status) => {
     : [];
 };
 
+/* istanbul ignore next */
 function dateValid(date) {
   try {
     return formatDate(date);
@@ -103,9 +104,17 @@ export default class {
   };
 
   handleEditTicket(e, bill, bills) {
+    console.log(this.id + " this");
+    console.log(bill.id + " bill");
+
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0;
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id;
+
     if (this.counter % 2 === 0) {
+      console.log("=====");
+      console.log("if");
+      console.log(this.counter + " if Edit");
+      console.log("=====");
       bills.forEach((b) => {
         $(`#open-bill${b.id}`).css({ background: "#0D5AE5" });
       });
@@ -114,13 +123,17 @@ export default class {
       $(".vertical-navbar").css({ height: "150vh" });
       this.counter++;
     } else {
+      console.log("=====");
+      console.log("else");
+      console.log(this.counter + " else Edit");
+      console.log("=====");
       $(`#open-bill${bill.id}`).css({ background: "#0D5AE5" });
 
       $(".dashboard-right-container div").html(`
         <div id="big-billed-icon"> ${BigBilledIcon} </div>
       `);
       $(".vertical-navbar").css({ height: "120vh" });
-      this.counter++;
+      this.counter--;
     }
     $("#icon-eye-d").click(this.handleClickIconEye);
     $("#btn-accept-bill").click((e) => this.handleAcceptSubmit(e, bill));
@@ -148,18 +161,23 @@ export default class {
   };
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0;
+    if (this.counter1 === undefined || this.index !== index) this.counter1 = 0;
     if (this.index === undefined || this.index !== index) this.index = index;
-    if (this.counter % 2 === 0) {
+
+    if ($(`#status-bills-container${this.index}`).children().length > 0) {
+      this.counter1 = 1;
+    }
+
+    if (this.counter1 % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: "rotate(0deg)" });
       $(`#status-bills-container${this.index}`).html(
         cards(filteredBills(bills, getStatus(this.index)))
       );
-      this.counter++;
+      this.counter1++;
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: "rotate(90deg)" });
       $(`#status-bills-container${this.index}`).html("");
-      this.counter++;
+      this.counter1--;
     }
 
     bills.forEach((bill) => {
@@ -192,6 +210,7 @@ export default class {
   };
 
   // not need to cover this function by tests
+  /* istanbul ignore next */
   updateBill = (bill) => {
     if (this.firestore) {
       return this.firestore
