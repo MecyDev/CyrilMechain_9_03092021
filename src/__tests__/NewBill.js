aa5088e03f145e66232182fbd904fe111201fc10
macmc <macmecy@gmail.com>
2021-09-16 15:27:40 +0200

    test ok and coverage upper then 80%

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -19,20 +19,37 @@ window.localStorage.setItem(
 );
 
 describe("Given I am connected as an employee", () => {
-  describe("When I access NewBill Page", () => {
+  describe("When I am on NewBill Page", () => {
     test("Then the newBill page should be rendered", () => {
       document.body.innerHTML = NewBillUI();
-      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
-    });
-    test("Then a form with nine fields should be rendered", () => {
-      document.body.innerHTML = NewBillUI();
-      const form = document.querySelector("form");
-      expect(form.length).toEqual(9);
+      expect(screen.getByTestId("new-bill").textContent).toEqual(
+        " Envoyer une note de frais "
+      );
     });
   });
   describe("When I'm on NewBill Page", () => {
-    describe("And I upload a image file", () => {});
-    describe("And I upload a non-image file", () => {
+    describe("And I upload a image file", () => {
+      test("Then the file handler should show a file", () => {
+        document.body.innerHTML = NewBillUI();
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firestore: firestore,
+          localStorage: window.localStorage,
+        });
+        const handleChangeFile = jest.fn(() => newBill.handleChangeFile);
+        const inputFile = screen.getByTestId("file");
+        inputFile.addEventListener("change", handleChangeFile);
+        fireEvent.change(inputFile, {
+          target: {
+            files: [new File(["test.txt"], "test.txt", { type: "text/txt" })],
+          },
+        });
+        const numberOfFile = screen.getByTestId("file").files.length;
+        expect(numberOfFile).toEqual(1);
+      });
+    });
+    describe("And I upload a wrong format file", () => {
       test("Then the error message should be display", async () => {
         document.body.innerHTML = NewBillUI();
         const newBill = new NewBill({
@@ -46,20 +63,16 @@ describe("Given I am connected as an employee", () => {
         inputFile.addEventListener("change", handleChangeFile);
         fireEvent.change(inputFile, {
           target: {
-            files: [
-              new File(["sample.txt"], "sample.txt", { type: "text/txt" }),
-            ],
+            files: [new File(["test.txt"], "test.txt", { type: "text/txt" })],
           },
         });
         expect(handleChangeFile).toBeCalled();
-        expect(inputFile.files[0].name).toBe("sample.txt");
-        expect(document.querySelector(".error-imageFormat").style.display).toBe(
-          "block"
-        );
+        expect(inputFile.files[0].name).toBe("test.txt");
+        expect(screen.getByTestId("error-img").style.display).toBe("inline");
       });
     });
-    describe("And I submit a valid bill form", () => {
-      test("then a bill is created", async () => {
+    describe("And I submit a valid bill", () => {
+      test("Then a bill is created", async () => {
         document.body.innerHTML = NewBillUI();
         const newBill = new NewBill({
           document,
@@ -68,34 +81,28 @@ describe("Given I am connected as an employee", () => {
           localStorage: window.localStorage,
         });
         const submit = screen.getByTestId("form-new-bill");
-        const validBill = {
-          name: "validBill",
-          date: "2021-01-01",
-          type: "Restaurants et bars",
-          amount: 10,
-          pct: 10,
-          vat: "40",
-          fileName: "test.jpg",
-          fileUrl: "https://goulven-clech.dev/test.jpg",
+        const bill = {
+          name: "Bill",
+          date: "2021-07-14",
+          type: "Transports",
+          amount: 99,
+          pct: 20,
+          vat: 70,
+          commentary: "bonjour le monde",
+          fileName: "test.png",
+          fileUrl: "https://google.com/test.png",
         };
         const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
         newBill.createBill = (newBill) => newBill;
-        document.querySelector(`input[data-testid="expense-name"]`).value =
-          validBill.name;
-        document.querySelector(`input[data-testid="datepicker"]`).value =
-          validBill.date;
-        document.querySelector(`select[data-testid="expense-type"]`).value =
-          validBill.type;
-        document.querySelector(`input[data-testid="amount"]`).value =
-          validBill.amount;
-        document.querySelector(`input[data-testid="vat"]`).value =
-          validBill.vat;
-        document.querySelector(`input[data-testid="pct"]`).value =
-          validBill.pct;
-        document.querySelector(`textarea[data-testid="commentary"]`).value =
-          validBill.commentary;
-        newBill.fileUrl = validBill.fileUrl;
-        newBill.fileName = validBill.fileName;
+        screen.getByTestId("expense-name").value = bill.name;
+        screen.getByTestId("datepicker").value = bill.date;
+        screen.getByTestId("expense-type").value = bill.type;
+        screen.getByTestId("amount").value = bill.amount;
+        screen.getByTestId("vat").value = bill.vat;
+        screen.getByTestId("pct").value = bill.pct;
+        screen.getByTestId("commentary").value = bill.commentary;
+        newBill.fileUrl = bill.fileUrl;
+        newBill.fileName = bill.fileName;
         submit.addEventListener("click", handleSubmit);
         fireEvent.click(submit);
         expect(handleSubmit).toHaveBeenCalled();
